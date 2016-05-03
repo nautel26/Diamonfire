508eae4e38ba3ebaec429adf6f0dbf8e307d72d0
trungnn142 <trungnn142@gmail.com>
2016-05-03 15:32:17 +0700

    Finish login logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
 
-  ATTRIBUTES = [:name, :email, :phone_number, :address, :province, :password,
+  attr_accessor :login
+
+  ATTRIBUTES = [:name, :username, :email, :phone_number, :address, :province, :password,
     :password_confirmation, :current_password]
+
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
